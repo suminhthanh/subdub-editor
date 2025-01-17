275dbfdbf495dbc4ed80a42202699ae90ddc1034
tsu <smthanh@gmail.com>
2025-01-17 23:50:46 +0700

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: "/subdub-editor/",
+    base: "/",
     plugins: [react()],
     server: {
       port: 3000,
